4dcf1f2839526b27090fa4c480b8f1d50c990525
Rijalul222 <56832201+Rijalul222@users.noreply.github.com>
2019-10-21 23:13:40 +0700

    Tampilan
    Tampilan

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,7 +2,7 @@
     <section class="content-header">
       <h1>
         Halaman 
-        <small>ADMIN</small>
+        <small>Admin</small>
       </h1>
       <ol class="breadcrumb">
         <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
